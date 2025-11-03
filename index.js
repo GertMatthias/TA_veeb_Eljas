7ca19021b1e6a6fa8c11408c71647c81897ff7e7
Eljas <GertMatthias@users.noreply.github.com>
2025-11-03 13:21:11 +0200

    Galerii

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 const express = require("express");
 const fs = require("fs");
-//pأ¤ringu lahtiharutaja POST jaoks
 const bodyparser = require("body-parser");
-const mysql = require("mysql2");
+//const mysql = require("mysql2/promise");
 const dateEt = require("./src/dateTimeET");
-const dbInfo = require("../../vp2025config");
+//const dbInfo = require("../../../../vp2025config");
 const textRef = "public/txt/vanasonad.txt";
-//kأ¤ivitan express.js funktsiooni ja annan talle nimeks "app"
 const app = express();
-//mأ¤أ¤ran veebilehtede mallide renderdamise mootori
 app.set("view engine", "ejs");
-//mأ¤أ¤ran أ¼he pأ¤ris kataloogi avalikult kأ¤ttesaadavaks
 app.use(express.static("public"));
-//parsime pأ¤ringu URL-i, lipp false, kui ainult tekst ja true, kui muid andmeid ka
-app.use(bodyparser.urlencoded({extended: false}));
-
-//loon andmebaasiühenduse
-const conn = mysql.createConnection({
-	host: dbInfo.configData.host,
-	user: dbInfo.configData.user,
-	password: dbInfo.configData.passWord,
-	database: dbInfo.configData.dataBase
-});
+//kui tuleb vormist ainult tekst, siis false, muidu true
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.get("/", (req, res)=>{
-	//res.send("Express.js lأ¤ks kأ¤ima ja serveerib veebi!");
 	res.render("index");
 });
 
@@ -38,12 +25,11 @@ app.get("/vanasonad", (req, res)=>{
 	let folkWisdom = [];
 	fs.readFile(textRef, "utf8", (err, data)=>{
 		if(err){
-			//kui tuleb viga, siis ikka vأ¤ljastame veebilehe, liuhtsalt vanasأµnu pole أ¼htegi
-			res.render("genericlist", {heading: "Valik Eesti vanasأµnu", listData: ["Ei leidnud أ¼htegi vanasأµna!"]});
+			res.render("genericlist", {heading: "Valik Eesti vanasõnu", listData: ["Ei leidnud ühtegi vanasõna!"]});
 		}
 		else {
 			folkWisdom = data.split(";");
-			res.render("genericlist", {heading: "Valik Eesti vanasأµnu", listData: folkWisdom});
+			res.render("genericlist", {heading: "Valik Eesti vanasõnu", listData: folkWisdom});
 		}
 	});
 });
@@ -54,7 +40,6 @@ app.get("/regvisit", (req, res)=>{
 
 app.post("/regvisit", (req, res)=>{
 	console.log(req.body);
-	//avan tekstifaili kirjutamiseks sellisel moel, et kui teda pole, luuakse (parameeter "a")
 	fs.open("public/txt/visitlog.txt", "a", (err, file)=>{
 		if(err){
 			throw(err);
@@ -78,7 +63,6 @@ app.get("/visitlog", (req, res)=>{
 	let listData = [];
 	fs.readFile("public/txt/visitlog.txt", "utf8", (err, data)=>{
 		if(err){
-			//kui tuleb viga, siis ikka vأ¤ljastame veebilehe, liuhtsalt vanasأµnu pole أ¼htegi
 			res.render("genericlist", {heading: "Registreeritud külastused", listData: ["Ei leidnud ühtegi külastust!"]});
 		}
 		else {
@@ -86,50 +70,18 @@ app.get("/visitlog", (req, res)=>{
 			let correctListData = [];
 			for(let i = 0; i < listData.length - 1; i ++){
 				correctListData.push(listData[i]);
-			}				
+			}
 			res.render("genericlist", {heading: "registreeritud külastused", listData: correctListData});
 		}
 	});
 });
 
-app.get("/Eestifilm", (req, res)=>{
-	res.render("eestifilm");
-});
-app.get("/Eestifilm/inimesed", (req, res)=>{
-	const sqlReq = "SELECT * FROM person";
-	conn.execute(sqlReq, (err, sqlres)=>{
-		if (err){
-			throw(err);
-		}
-		else {
-			console.log(sqlres);
-			res.render("filmiinimesed", {personList: sqlres});
-		}
-	});
-	//res.render("filmiinimesed");
-});
+//Eesti filmi marsruudid
+const eestifilmRouter = require("./routes/eestifilmRoutes");
+app.use("/Eestifilm", eestifilmRouter);
 
-app.get("/Eestifilm/filmiinimesed_add", (req, res)=>{
-	res.render("filmiinimesed_add", {notice: "Ootan sisestust"});
-});
-
-app.post("/Eestifilm/filmiinimesed_add", (req, res)=>{
-	console.log(req.body);
-	//kas andmed on olemas
-	if(!req.body.firstNameInput || !req.body.lastNameInput || !req.body.bornInput || req.body.bornInput >= new Date()){
-		res.render("filmiinimesed_add", {notice: "Osa andmeid oli puudu või ebakorrektsed"});
-	}
-	else {
-		let sqlReq = "INSERT INTO person (first_name, last_name, born, deceased) VALUES (?,?,?,?)";
-		conn.execute(sqlReq, [req.body.firstNameInput, req.body.lastNameInput, req.body.bornInput, req.body.deceasedInput], (err, sqlres)=>{
-			if(err){
-				res.render("filmiinimesed_add", {notice: "Andmete salvestamine ebaõnnestus"});
-			}
-			else {
-				res.render("filmiinimesed_add", {notice: "Andmed salvestatud"});
-			}
-		});
-	}
-});
+//Galeriifotode üleslaadimine
+const photoupRouter = require("./routes/photoupRoutes");
+app.use("/galleryphotoupload", photoupRouter);
 
 app.listen(5116);
